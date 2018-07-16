5487e47cece2444a8da84fb33994fc4cfe462e3a
nuest <daniel.nuest@uni-muenster.de>
2018-07-16 11:53:48 +0200

    add intro presentation

---
+++ 2018-08_intro/macros.js
@@ -0,0 +1,13 @@
+remark.macros.scale = function (percentage) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + percentage + '" />';
+};
+
+remark.macros.inline_highlight = function () {
+  var word = this;
+  return '<span class="inline_highlight">' + word + '</span>';
+};
+remark.macros.highlight_inline = function () {
+  var word = this;
+  return '<span class="inline_highlight">' + word + '</span>';
+};
